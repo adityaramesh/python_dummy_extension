5a202835e57a4b671b117e788c0690cfe698bf0c
Aditya Ramesh <_@adityaramesh.com>
2016-02-27 04:58:54 -0500

    Fixed issue with shared library not being copied.

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
 	name         = 'dummy',
 	version      = '0.1',
 	description  = 'An example Python extension module written in C++.',
-	url          = '',
+	url          = 'https://github.com/adityaramesh/python_dummy_extension',
 	author       = 'Aditya Ramesh',
 	author_email = '_@adityaramesh.com',
 	license      = 'BSD',
-	packages     = ['dummy']
+	packages     = ['dummy'],
+	package_data = {'dummy': ['_dummy.so']}
 )

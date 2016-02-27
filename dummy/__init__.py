5566e666fcfe5231e8283c0d4203a0c92e5b598c
Aditya Ramesh <_@adityaramesh.com>
2016-02-27 05:00:48 -0500

    Fixed issue with shared library not being copied.

--- dummy/__init__.py
+++ dummy/__init__.py
@@ -1,1 +1,4 @@
-from dummy._dummy import __doc__, make_message, reverse
+try:
+	from _dummy import __doc__, make_message, reverse
+except ImportError:
+	from dummy._dummy import __doc__, make_message, reverse
